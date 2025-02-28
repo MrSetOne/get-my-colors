caa7d716fd5d913569b29ee5796c194f9c6bb736
Michael Lara Sánchez <lara.sanchez.michael@gmail.com>
2025-02-28 19:29:12 +0100

    test: isValidHexColor fails tests

--- src/__tests__/validators.test.ts
+++ src/__tests__/validators.test.ts
@@ -1,3 +1,5 @@
+import { generateHexValue } from "@/__tests__/testUtils/generators";
+import { VALID_HEX_LENGHT_CASES } from "@/constants/hex.constants";
 import { isValidHexColor } from "@/validators";
 import { describe, it, expect } from "vitest";
 
@@ -5,5 +7,31 @@ describe("isValidHexColor", () => {
   it("should exist", () => {
     const type = typeof isValidHexColor;
     expect(type).toBe("function");
+  });
+
+  it("Sould return false if the color not starts with #", () => {
+    const color = generateHexValue(false, false, false);
+    const result = isValidHexColor(color);
+    expect(result).toBe(false);
+  });
+
+  it("Should return false if the color has incorrect length (x1000)", () => {
+    for (let i = 0; i < 1000; i++) {
+      const color = generateHexValue(false, false);
+      const result = isValidHexColor(color);
+      expect(result).toBe(false);
+    }
+  });
+
+  it("Should return false if the color has incorrect values (x1000)", () => {
+    for (let i = 0; i < 1000; i++) {
+      const targetLengthIndex = Math.floor(
+        Math.random() * VALID_HEX_LENGHT_CASES.length
+      );
+      const targetLength = VALID_HEX_LENGHT_CASES[targetLengthIndex];
+      const color = generateHexValue(false, targetLength);
+      const result = isValidHexColor(color);
+      expect(result).toBe(false);
+    }
   });
 });
